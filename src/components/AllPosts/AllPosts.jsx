0e7af72c1a0129b602cd9cb897a1c24f85edd71c
Andrey Moroz <atmoroz93@gmail.com>
2020-04-27 22:13:06 +0300

    add linter and prettier

--- src/components/AllPosts/AllPosts.jsx
+++ src/components/AllPosts/AllPosts.jsx
@@ -1,6 +1,5 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-
 
 import AllPostsContainer from './AllPostsContainer';
 
@@ -8,38 +7,41 @@ import { requestAllPosts } from '../../store/action/action';
 import './allPosts.css';
 import Loading from '../Loading/Loading';
 const AllPosts = () => {
-	const dispatch = useDispatch();
-	const {isLoading, Posts} = useSelector(store => store);
+  const dispatch = useDispatch();
+  const { isLoading, Posts } = useSelector(store => store);
 
-	useEffect(() => {
-		dispatch(requestAllPosts());
-	}, [dispatch]);
+  useEffect(() => {
+    dispatch(requestAllPosts());
+  }, [dispatch]);
 
-	return(
-		<>
-			<h2 className='title'>Posts</h2>
-			<div className='postWrapper'>
-			{
-				isLoading ? <Loading css={{    position: 'absolute',
-														 top: 'calc(50% - 35px)',
-														 bottom: '0',
-														 left: 'calc(50% - 35px)',
-														 right: '0',
-														}}
-				/> :
-				Posts.map(post => (
-					<AllPostsContainer
-						key={post.id}
-						title={post.title}
-						body={post.body}
-						id={post.id}
-						userId={post.userId}
-						spin={post.spin ? post.spin : null}
-					/>
-				))
-			}
-			</div>
-		</>
-	)
-}
+  return (
+    <>
+      <h2 className="title">Posts</h2>
+      <div className="postWrapper">
+        {isLoading ? (
+          <Loading
+            css={{
+              position: 'absolute',
+              top: 'calc(50% - 35px)',
+              bottom: '0',
+              left: 'calc(50% - 35px)',
+              right: '0',
+            }}
+          />
+        ) : (
+          Posts.map(post => (
+            <AllPostsContainer
+              key={post.id}
+              title={post.title}
+              body={post.body}
+              id={post.id}
+              userId={post.userId}
+              spin={post.spin ? post.spin : null}
+            />
+          ))
+        )}
+      </div>
+    </>
+  );
+};
 export default AllPosts;
--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -1,11 +1,7 @@
 import React from 'react';
-import ClipLoader from "react-spinners/ClipLoader";
+import ClipLoader from 'react-spinners/ClipLoader';
 
-const Loading = ({css}) => {
-	return(
-		<ClipLoader
-			css={{...css}}
-		/>
-	)
-}
+const Loading = ({ css }) => {
+  return <ClipLoader css={{ ...css }} />;
+};
 export default Loading;
